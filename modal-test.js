2836bae2050850eecfad68015c84c1da6af034ab
Gavia1981 <j_sodercrantz@hotmail.com>
2016-06-03 15:43:28 +0200

    Update modal-test.js

--- modal-test.js
+++ modal-test.js
@@ -1,26 +1,91 @@
 (function parseTodaysSightings(userAlias) {
-    var Templates = {};
 
-    Templates.modal = $([
+    var vm = this; 
+    vm.sightings = [];
+    vm.userAlias = userAlias;
+    vm.extractionActivated = true;
+    vm.missingSpeciesArray = [];
+    vm.sightingSubscription = null;
+
+    vm.Templates = {};
+    vm.Templates.iFrame = $("<iframe />").appendTo(document.body).hide();
+    vm.Templates.modal = $([
         '<div class="modal hide in" id="extractSightingsModal">',
             '<div class="modal-header">',
                 '<button type="button" class="close" data-dismiss="modal"><span>&times;</span></button>',
-                '<h4 class="modal-title">Letar kryss</h4>',
+                '<span class="modal-title"><b>0</b> fynd hittades</span>',
             '</div>',
             '<div class="modal-body">',
-                '<div class="tabbable">',
-                    '<ul class="nav nav-tabs" id="smalltabs">',
-                        '<li class="active"><a href="#test1" data-toggle="tab">3 juni</a></li>',
-                        '<li><a href="#test2" data-toggle="tab" class=" btn-map">2 juni</a></li>',
-                    '</ul>',
-                    '<div class="tab-content">',
-                        '<div class="tab-pane active" id="test1"></div>',
-                    '</div>',
-                '</div>',
+                '<img src="https://artportalen.se/Content/Images/ajax-loader-circle.gif"> Letar kryss...',
             '</div>',
         '</div>'
-    ].join("\n"));
+    ].join("\n")).appendTo(document.body).modal("show").find("button.close").click(function() {
+        vm.extractionActivated = false;
+        vm.sightingSubscription.dispose();
+        vm.Templates.iFrame.remove();
+    }).end();
 
-    Templates.modal.appendTo(document.body).modal("show");
+    vm.pageForward = function() {
+        if (!vm.extractionActivated) return false;
+        if (window.viewModel.pageIndex() != window.viewModel.maxPageIndex() && window.viewModel.maxPageIndex() > 0) {
+            window.viewModel.setPageIndex(window.viewModel.pageIndex() + 1, true);
+        } else if (window.viewModel.dayNamesBackCount() > 0) {
+            $("#btn-daypager .daypager-back").click();
+        } else {
+            vm.extractionActivated = false;
+        }
+    };
+
+    vm.getListUrl = function() {
+        return [
+            "https://artportalen.se/List/Top/Species/F%C3%A5glar/Total", 
+            window.viewModel.selectedAreaIsMunicipality() ? "Kommun" : "Landskap", 
+            window.viewModel.selectedAreaName(), 
+            "AnySite/OrderByTaxon/Asc", 
+            vm.userAlias
+        ].join('/') + "?t=" + new Date().getTime();
+    };
+
+    vm.extractSightings = function() {
+        return $.grep($("#todayssightings tr.sightingrow"), function( obj, index ) {
+            return $.inArray($(obj).find("span[data-taxonid]").data("taxonid"), vm.missingSpeciesArray) === -1;
+        });
+    };
+
+    vm.displaySightings = function() {
+        if (!vm.extractionActivated) return false;
+        vm.sightings = vm.extractSightings();
+        if (window.viewModel.pageIndex() == 1) {
+            var dateDisplay = window.viewModel.selectedDayName().PresentationDateDayMonthShort + " " + new Date(parseInt(window.viewModel.selectedDayName().Date.substr(6))).getUTCFullYear();
+            vm.Templates.modal.find("#extractedsightings").append("<tr class='divider'><td colspan='3'><h4 style='margin: 1em 0 0;border-bottom: 1px solid #999;'>" + dateDisplay + "</h4></td></tr>");
+        }
+        vm.Templates.modal.find("#extractedsightings").append(vm.sightings);
+        vm.Templates.modal.find(".modal-title b").text(vm.Templates.modal.find("#extractedsightings tr:not(.divider)").length);
+        //vm.pageForward();
+    };
+
+    vm.init = function() {
+        window.viewModel.setPageIndex(1, true);
+        vm.Templates.iFrame.attr("src", vm.getListUrl()).load(function() {
+
+            vm.Templates.modal.find(".modal-body").html("<table id='extractedsightings'></table>");
+
+            vm.missingSpeciesArray = vm.Templates.iFrame.contents().find("span[data-taxonid]").map(function() {
+                return $(this).data("taxonid");
+            }).get();
+
+            vm.displaySightings();
+
+            // Subscribe to changes of sightings (eg. loading new ones)
+            vm.sightingSubscription = window.viewModel.sightings.subscribe(function(newValue) {
+                if (!vm.extractionActivated) return false;
+                setTimeout(function() {
+                    vm.displaySightings();
+                }, 300);
+
+            });
+        });
+    };
+    vm.init();
 
 })("johans");
