eccb3b9518e8fe8ffc5126eb05572182c6fe7176
Gavia1981 <j_sodercrantz@hotmail.com>
2016-06-22 22:32:33 +0200

    Update kommunkryss.js

--- kommunkryss.js
+++ kommunkryss.js
@@ -1,22 +1,14 @@
 (function kommuner() {
         
-        window.onerror = function(msg, url, line, col, error) {
-           // Note that col & error are new to the HTML 5 spec and may not be 
-           // supported in every browser.  It worked for me in Chrome.
-           var extra = !col ? '' : '\ncolumn: ' + col;
-           extra += !error ? '' : '\nerror: ' + error;
-        
-           // You can view the information in an alert to see things working like this:
-           alert("Error: " + msg + "\nurl: " + url + "\nline: " + line + extra);
-        
-           // TODO: Report this error via ajax so you can keep track
-           //       of what pages have JS issues
-        
-           var suppressErrorAlert = true;
-           // If you return true, then error alerts (like in older versions of 
-           // Internet Explorer) will be suppressed.
-           return suppressErrorAlert;
-        };
+        // display error messages for a page, but never more than 3 errors
+        window.onerror = function(msg, url, line) {
+                if (onerror.num++ < onerror.max) {
+                        alert("ERROR: " + msg + "\n" + url + ":" + line);
+                        return true;
+                }
+        }
+        onerror.max = 3;
+        onerror.num = 0;
 
         /**
          * fastLiveFilter jQuery plugin 1.0.3
