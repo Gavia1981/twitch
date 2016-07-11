1f60988b753fe81278ada34340faadf826d7fbd7
Gavia1981 <j_sodercrantz@hotmail.com>
2016-07-11 17:40:23 +0200

    Update ebird.js

--- ebird.js
+++ ebird.js
@@ -33,7 +33,7 @@
     }).end();
 
 	vm.stripSpeciesName = function(speciesName ) {
-		return speciesName.replace(/\(hybrid\)|\(viridis\/karelini\)|\(canus\)|\(hirundo\)|\(merganser\/orientalis\)|\(alba\/dukhunensis\)|\[major Group\]|\/vitbröstad skarv|\(carbo\)|\/australisk fiskgjuse|\(haliaetus\)|\/jakutisk nötväcka/gi,"");
+		return speciesName.replace(/\(hybrid\)|\(viridis\/karelini\)|\/grön fasan|\(canus\)|\(hirundo\)|\(merganser\/orientalis\)|\(alba\/dukhunensis\)|\[major Group\]|\/vitbröstad skarv|\(carbo\)|\/australisk fiskgjuse|\(haliaetus\)|\/jakutisk nötväcka/gi,"");
 	};
 
 	function addHours(d, h) {
